93a2e5e3b7b0889ea10cbd11a260f7b296c05561
Shakib-Shazzadul <shakib1186@gmail.com>
2024-09-23 16:02:19 +0600

    origin updated

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://z-bot-client.vercel.app"],
     credentials: true,
   })
 );
